4874b1b05e23fe5611dd7b25249cd7d9d9666816
Mohammad Arfizur Rahman <arfiz0@gmail.com>
2020-05-23 17:13:18 +0600

    Fetching weather app via comman line

---
+++ complete-node-developer/playground/4-callbacks.js
@@ -0,0 +1,24 @@
+// const geocode = (address, callback) => {
+// 	setTimeout(() => {
+// 		const data = {
+// 			lat: 0,
+// 			long: 0,
+// 		};
+
+// 		callback(data);
+// 	}, 2000);
+// };
+
+// geocode('asdad', (data) => {
+// 	console.log(data);
+// });
+
+const add = (num1, num2, callback) => {
+	setTimeout(() => {
+		callback(num1 + num2);
+	}, 2000);
+};
+
+add(1, 4, (sum) => {
+	console.log(sum);
+});
