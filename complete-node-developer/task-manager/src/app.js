98f0fc5a072cf105602564b8a91bcfadf0bc6bae
Mohammad Arfizur Rahman <arfiz0@gmail.com>
2020-05-26 23:46:53 +0600

    Completing testing the task app

---
+++ complete-node-developer/task-manager/src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('./db/mongoose');
+
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
+
+const app = express();
+
+// app.use((req, res, next) => {
+// 	res.status(503).send('Site is currently down. Check back soon!');
+// });
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+// const Task = require('./models/task');
+// const User = require('./models/user');
+
+// const main = async () => {
+// 	const task = await Task.findById('5ecc83f16ebfe72684a36301');
+// 	await task.populate('owner').execPopulate();
+
+// 	const user = await User.findById('5ecc1bbfa41b1c24941b6c0b');
+// 	await user.populate('tasks').execPopulate();
+// };
+
+// main()
+
+module.exports = app;
