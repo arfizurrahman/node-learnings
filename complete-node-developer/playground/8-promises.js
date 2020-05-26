93eed2d7ac66b13f28dfa6bdb2e579fcaf3cd931
Mohammad Arfizur Rahman <arfiz0@gmail.com>
2020-05-26 10:21:01 +0600

    Completing authentication, pagination, sorting, filtering

--- complete-node-developer/playground/8-promises.js
+++ complete-node-developer/playground/8-promises.js
@@ -1,19 +1,55 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve([1, 2, 4]);
-	}, 2000);
-});
+// const doWorkPromise = new Promise((resolve, reject) => {
+// 	setTimeout(() => {
+// 		resolve([1, 2, 4]);
+// 	}, 2000);
+// });
 
-doWorkPromise
-	.then((result) => {
-		console.log('Success', result);
-	})
-	.catch((err0r) => {
-		console.log('Fail', error);
-	});
+// doWorkPromise
+// 	.then((result) => {
+// 		console.log('Success', result);
+// 	})
+// 	.catch((err0r) => {
+// 		console.log('Fail', error);
+// 	});
 
 //                        fulfilled
 //                       /
 // Promise -- pending ->
 //                       \
 //                        rejected
+
+const add = (a, b) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(a + b);
+		}, 2000);
+	});
+};
+
+// add(1, 2)
+// 	.then((sum) => {
+// 		console.log(sum);
+// 		add(sum, 2)
+// 			.then((sum) => {
+// 				console.log(sum);
+// 			})
+// 			.catch((err) => {
+// 				console.log(err);
+// 			});
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
+
+add(1, 2)
+	.then((sum) => {
+		console.log(sum);
+
+		return add(sum, 2);
+	})
+	.then((sum2) => {
+		console.log(sum2);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
